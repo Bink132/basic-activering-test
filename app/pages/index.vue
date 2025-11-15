20c4a5963bc5e27664fbf7b5ae960b2f16c3ccfb
Bink <2207723@talnet.nl>
2025-11-15 15:50:29 +0100

    Update images and HTML paths

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,11 +5,11 @@
       subtitle="De beste keuze voor maatwerk rolstoelen"
       buttonText="Bekijk onze modellen">
     </slideshow>
-    <SectionIntro    
-     image="/basic-activering-test/img/Home/picknicktafel.jpg"
+    <SectionIntro
+      image="/img/home/picknicktafel.jpg"
       alt-text="picknicktafel"
       text="Basic Activering zorgt voor dagbesteding voor specifieke doelgroepen op onze vestigingen in Amsterdam West en IJburg. Op deze locaties zijn onze deskundigen constant bezig zijn met het helpen van doelgroepen met een achterstand op de arbeidsmarkt. Dit kan zijn door een lichamelijke of verstandelijke beperking of mensen met een indicatie banenafspraak. Onze dagbestedingsprojecten richten zich op laagdrempelige activiteiten die het zelfvertrouwen van deze specifieke doelgroepen stimuleren, zodat zij op den duur ook een goede plek op de arbeidsmarkt krijgen."
-    ></SectionIntro>
+    />
     <SectionCTA
     image="/img/home/Bed-hout-1.png"
       alt-text="Bed hout"
@@ -23,3 +23,4 @@
     <SectionPartners></SectionPartners>
   </div>
 </template>
+
