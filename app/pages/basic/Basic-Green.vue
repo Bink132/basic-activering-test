c0ff006dbdc818b5232c16b6d16acb9787d83f7e
Bink <2207723@talnet.nl>
2025-11-16 09:59:29 +0100

    Update images and HTML paths for basic pages

--- app/pages/basic/Basic-Green.vue
+++ app/pages/basic/Basic-Green.vue
@@ -4,15 +4,28 @@
     <SectionIntro image="/basic-activering-test/img/home/picknicktafel.jpg" alt-text="picknicktafel"
         text="Wil je als dagbesteding tuinwerkzaamheden verrichten onder begeleiding van experts in Amsterdam? Bij Basic Activering werk je met een lach! Basic Green is een project van ons waar je in een kleinschalige, beschutte omgeving tuinwerkzaamheden verricht en een tuin leert onderhouden onder professionele begeleiding.">
     </SectionIntro>
-    <SectionCTA image="/basic-activering-test/img/home/Bed-hout-1.png" alt-text="Bed hout" title="Benieuwd wat wij nog meer doen?"
-        button-text="Bekijk onze diensten"></SectionCTA>
+    <SectionCTA image="/basic-activering-test/img/home/Bed-hout-1.png" alt-text="Bed hout"
+        title="Benieuwd wat wij nog meer doen?" button-text="Bekijk onze diensten"></SectionCTA>
     <section class="bg-blue-700 text-white my-20 mx-20 py-5 px-5">
         <h1 class="text-2xl font-bold">Tuinwerkzaamheden die onder onze begeleiding zijn uitgevoerd</h1>
-        <p>Als je als dagbesteding tuinwerkzaamheden wilt uitvoeren onder onze begeleiding, ga je aan de slag in volkstuincomplexen. Daar gaan we bijvoorbeeld terrassen aanleggen en tuinen winter- of zomer klaar maken. Ook leer je onder onze begeleiding hoe je een tuin kunt onderhouden, door schuren, kappingen en schuttingen te plaatsen en meer. Dit soort tuinonderhoud voeren we onder andere uit bij sportverenigingen, maar ook bij particulieren. Als je graag in de buitenlucht bezig bent, is het uitvoeren van tuinwerkzaamheden een ideale dagbesteding voor jou. Werk je graag met je handen? Bekijk dan ook onze technische activiteiten. Als een tuin leren onderhouden niet bij je past en je niet zeker weet welke van onze opties je wel wilt, volg je ons Basic Totaalprogramma.</p>
+        <p>Als je als dagbesteding tuinwerkzaamheden wilt uitvoeren onder onze begeleiding, ga je aan de slag in
+            volkstuincomplexen. Daar gaan we bijvoorbeeld terrassen aanleggen en tuinen winter- of zomer klaar maken.
+            Ook leer je onder onze begeleiding hoe je een tuin kunt onderhouden, door schuren, kappingen en schuttingen
+            te plaatsen en meer. Dit soort tuinonderhoud voeren we onder andere uit bij sportverenigingen, maar ook bij
+            particulieren. Als je graag in de buitenlucht bezig bent, is het uitvoeren van tuinwerkzaamheden een ideale
+            dagbesteding voor jou. Werk je graag met je handen? Bekijk dan ook onze technische activiteiten. Als een
+            tuin leren onderhouden niet bij je past en je niet zeker weet welke van onze opties je wel wilt, volg je ons
+            Basic Totaalprogramma.</p>
     </section>
     <section class="bg-blue-700 text-white px-96 py-10">
         <h1 class="text-2xl font-bold flex justify-center">Voer tuinwerkzaamheden uit als dagbesteding</h1>
-        <p>Als je interesse hebt in tuinwerkzaamheden als dagbesteding, meld jij je aan om te leren tuinieren onder onze begeleiding. Onze dagbesteding is een unieke kans om je te verdiepen in de wereld van het tuinieren, nieuwe mensen te ontmoeten en een bijdrage te leveren aan een groenere omgeving. Door tuinwerkzaamheden te verrichten onder begeleiding, word je onderdeel van een creatief en productief proces waarin je je eigen talenten en passies kunt ontdekken en ontwikkelen. Een tuin onderhouden onder begeleiding biedt niet alleen een zinvolle tijdsbesteding, maar is ook een stap naar persoonlijke groei en tevredenheid. Meld je aan door contact met ons op te nemen. Wie weet doe je binnenkort aan dagbesteding op onze werkplaats.</p>
+        <p>Als je interesse hebt in tuinwerkzaamheden als dagbesteding, meld jij je aan om te leren tuinieren onder onze
+            begeleiding. Onze dagbesteding is een unieke kans om je te verdiepen in de wereld van het tuinieren, nieuwe
+            mensen te ontmoeten en een bijdrage te leveren aan een groenere omgeving. Door tuinwerkzaamheden te
+            verrichten onder begeleiding, word je onderdeel van een creatief en productief proces waarin je je eigen
+            talenten en passies kunt ontdekken en ontwikkelen. Een tuin onderhouden onder begeleiding biedt niet alleen
+            een zinvolle tijdsbesteding, maar is ook een stap naar persoonlijke groei en tevredenheid. Meld je aan door
+            contact met ons op te nemen. Wie weet doe je binnenkort aan dagbesteding op onze werkplaats.</p>
     </section>
     <WieVoorJou></WieVoorJou>
     <SectionLocations></SectionLocations>
--- app/pages/basic/Basic-totaal.vue
+++ app/pages/basic/Basic-totaal.vue
@@ -3,8 +3,8 @@
     <SectionIntro image="/basic-activering-test/img/home/picknicktafel.jpg" alt-text="picknicktafel"
         text="Basic Totaal is een project waar wij de deelnemers onder professionele begeleiding een variatie aan werkzaamheden uitvoeren. Uit het aanbod van de diverse beroepen waar de deelnemers bij assisteren komt ervan zelf uit waar hun passie ligt.">
     </SectionIntro>
-    <SectionCTA image="/basic-activering-test/img/home/Bed-hout-1.png" alt-text="Bed hout" title="Benieuwd wat wij nog meer doen?"
-        button-text="Bekijk onze diensten"></SectionCTA>
+    <SectionCTA image="/basic-activering-test/img/home/Bed-hout-1.png" alt-text="Bed hout"
+        title="Benieuwd wat wij nog meer doen?" button-text="Bekijk onze diensten"></SectionCTA>
     <WieVoorJou class="mt-10"></WieVoorJou>
     <SectionLocations></SectionLocations>
 </template>
